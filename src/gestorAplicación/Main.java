d6c1797beeee85a3ffc5ed76680cddef33e998b1
tscobarr <124937392+tscobarr@users.noreply.github.com>
2023-04-05 15:25:41 -0500

    commit 1 Metas

--- src/gestorAplicación/Main.java
+++ src/gestorAplicación/Main.java
@@ -2,14 +2,21 @@ package gestorAplicación;
 
 import baseDatos.*;
 
+import java.text.ParseException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
 		Usuario u1 = new Usuario("13123123", "Juan1@gmail.com", "121212312", 3);
 		System.out.println(Serializador.serializar(u1));
 		Usuario u13 = (Usuario) Deserializador.deserializar("Usuario");
 		System.out.println(u13.getNombre());
-	
+		
+		
+		Metas me1 = new Metas("Comprar una casa", 50000.0, "27/10/2025");
+		System.out.println(Serializador.serializar(me1));
+		Metas me2 = (Metas) Deserializador.deserializar("Metas");
+		System.out.println(me2.getFecha());
 	}
-
 }
--- src/gestorAplicación/Metas.java
+++ src/gestorAplicación/Metas.java
@@ -1,6 +1,58 @@
 package gestorAplicación;
 
-public class Metas {
+import java.io.Serializable;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class Metas implements Serializable{
+	private static final long serialVersionUID = 5L;
 	public static String nombreD = "Metas";
+	public String nombre;
+	protected double cantidad;
+	private Date fecha;
+	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+	
+
+// Constructor
+	public Metas(String nombre, double cantidad, String fecha) throws ParseException{
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.fecha = DATE_FORMAT.parse(fecha);
+	}
+
+	@Override
+	protected void finalize() {
+        System.out.println("La meta con nombre " + this.getNombre() + " y cantidad " + this.getCantidad() + 
+        		" para la fecha " + this.getFecha() + " fue eliminado satisfactoriamente del sistema.");
+	}
+
+// Getter y Setter
+	public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+	public double getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 
 }
