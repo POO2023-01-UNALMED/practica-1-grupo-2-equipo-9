e711c74c8580db8e530add0fcaa9f91b1050b493
DanielMor <jmorenoce@unal.edu.co>
2023-04-06 20:41:38 -0500

    atributos, gets y sets
    Se creó los componentes básicos de las clases Estado y Banco.

--- src/gestorAplicación/Banco.java
+++ src/gestorAplicación/Banco.java
@@ -2,5 +2,27 @@ package gestorAplicación;
 
 public class Banco extends Estado {
 	private static final long serialVersionUID = 2L;
-
+	private String nombreb;
+	private double comision;
+	
+	//Constructor
+	public Banco(String nombre, double tasa_interes, double tasa_impuestos, String nombreb, double comision) {
+		super(nombre, tasa_interes, tasa_impuestos);
+		this.nombreb = nombreb;
+		this.comision = comision;
+	}
+	//Gets
+	public double getComision() {
+		return comision;
+	}
+	public String getNombreb() {
+		return nombreb;
+	}
+	//Sets
+	public void setComision(double comision) {
+		this.comision = comision;
+	}
+	public void setNombreb(String nombreb) {
+		this.nombreb = nombreb;
+	}
 }
--- src/gestorAplicación/Estado.java
+++ src/gestorAplicación/Estado.java
@@ -5,5 +5,36 @@ import java.io.Serializable;
 public class Estado implements Serializable {
 	private static final long serialVersionUID = 1L;
 	public static String nombreD = "Estados";
+	private String nombre;
+	private double tasa_interes;
+	private double tasa_impuestos;
+	
+	//Constructor
+	public Estado(String nombre, double tasa_interes, double tasa_impuestos) {
+		this.nombre = nombre;
+		this.tasa_interes = tasa_interes;
+		this.tasa_impuestos = tasa_impuestos;
+	}
+	
+	//Gets
+	public String getNombre() {
+		return nombre;
+	}
+	public double getTasa_interes() {
+		return tasa_interes;
+	}
+	public double getTasa_impuestos() {
+		return tasa_impuestos;
+	}
+	//Sets
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setTasa_interes(double tasa_interes) {
+		this.tasa_interes = tasa_interes;
+	}
+	public void setTasa_impuestos(double tasa_impuestos) {
+		this.tasa_impuestos = tasa_impuestos;
+	}
 
 }
