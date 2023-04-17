64b5e7c1bbc3f9019e47e3d58a7a43b33f91fa3d
DanielMor <jmorenoce@unal.edu.co>
2023-04-16 19:56:59 -0500

    Update Banco.java
    Algunas cosas David necesitaba para uno de sus métodos

--- src/gestorAplicación/Banco.java
+++ src/gestorAplicación/Banco.java
@@ -1,19 +1,27 @@
 package gestorAplicación;
 
+	import java.util.ArrayList;
+
 public class Banco extends Estado {
 	private static final long serialVersionUID = 2L;
 	public static final String nombreD = "Bancos";
 	private String nombreb;
 	private double comision;
+	private static ArrayList<Banco> bancos = new ArrayList<Banco>();
 	
 	//Constructor
-	public Banco(String nombre, double tasa_interes, double tasa_impuestos, String nombreb, double comision) {
+	public Banco(String nombre, double tasa_interes, double tasa_impuestos, String nombreb, double comision, ArrayList<Banco> bancos) {
 		super(nombre, tasa_interes, tasa_impuestos);
 		this.nombreb = nombreb;
 		this.comision = comision;
+		bancos.add(this);
 	}
 	
-	public Banco() {
+	//Métodos
+	public void imprimirBancos() {
+		for (int i = 0; i < bancos.size(); i++) {
+			System.out.print(i+1 + ". " + bancos.get(i).getNombre());
+		}
 	}
 	
 	//Gets
@@ -23,6 +31,10 @@ public class Banco extends Estado {
 	public String getNombreb() {
 		return nombreb;
 	}
+
+	public ArrayList<Banco> getBancos() {
+		return bancos;
+	}
 	
 	//Sets
 	public void setComision(double comision) {
@@ -30,5 +42,10 @@ public class Banco extends Estado {
 	}
 	public void setNombreb(String nombreb) {
 		this.nombreb = nombreb;
+	}
+
+
+	public void setBancos(ArrayList<Banco> bancos) {
+		this.bancos = bancos;
 	}
 }
