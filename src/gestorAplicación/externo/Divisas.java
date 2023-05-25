bc2f5bd6840530324ffecd81f4efd950651b1201
Juanpa0128j <Juanpa0128j@gmail.com>
2023-05-24 21:28:02 -0500

    Revert "Merge branch 'main' of https://github.com/POO2023-01-UNALMED/practica-1-grupo-2-equipo-9"
    This reverts commit 25fe7b1cd3390d0269b81a40bfc629f498e1abbc, reversing
    changes made to 31e72fefcbad99cafba107818515576af4284517.

--- src/gestorAplicación/externo/Divisas.java
+++ src/gestorAplicación/externo/Divisas.java
@@ -5,13 +5,19 @@ import java.util.ArrayList;
 public enum Divisas {
 	EUR,
 	USD,
-	COP,;
+	YEN,
+	COP,
+	GBP,
+	MXN;
 	
 	public static ArrayList<Divisas> getDivisas() {
 		ArrayList<Divisas> listaDivisas = new ArrayList<Divisas>();
 		listaDivisas.add(EUR);
 		listaDivisas.add(USD);
+		listaDivisas.add(YEN);
 		listaDivisas.add(COP);
+		listaDivisas.add(GBP);
+		listaDivisas.add(MXN);
 		return(listaDivisas); 
 	}
 }
