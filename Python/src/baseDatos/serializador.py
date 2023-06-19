2f8d1b8d98f9df7c0b0b4e3e2498a5ed47f866d5
Juanpa0128j <Juanpa0128j@gmail.com>
2023-06-19 10:08:56 -0500

    Arreglo de serializado

--- Python/src/baseDatos/serializador.py
+++ Python/src/baseDatos/serializador.py
@@ -1,4 +1,5 @@
 import pickle as pk
+import os
 from gestorAplicación.interno.usuario import Usuario
 from gestorAplicación.interno.cuenta import Cuenta
 from gestorAplicación.interno.metas import Metas
@@ -7,30 +8,35 @@ from gestorAplicación.externo.estado import Estado
 from gestorAplicación.externo.banco import Banco
 
 class Serializador():
+
+    # Obtener ruta relativa
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    route_db = os.path.join(current_directory + '\\temp')
+
     #Método para serializar las listas con objetos de cualquier clase
     @classmethod
     def serializar(cls, lista_objetos) -> None:
         if(isinstance(lista_objetos[0], Usuario)):
-            objects_file = open("src/baseDatos/temp/lista_usuarios.pkl", "wb")
+            objects_file = open(cls.route_db + "\lista_usuarios.pkl", "wb")
             pk.dump(lista_objetos, objects_file)
             objects_file.close()
         if(isinstance(lista_objetos[0], Banco)):
-            objects_file = open("src/baseDatos/temp/lista_bancos.pkl", "wb")
+            objects_file = open(cls.route_db + "\lista_bancos.pkl", "wb")
             pk.dump(lista_objetos, objects_file)
             objects_file.close()
         if(isinstance(lista_objetos[0], Estado)):
-            objects_file = open("src/baseDatos/temp/lista_estados.pkl", "wb")
+            objects_file = open(cls.route_db + "\lista_estados.pkl", "wb")
             pk.dump(lista_objetos, objects_file)
             objects_file.close()
         if(isinstance(lista_objetos[0], Cuenta)):
-            objects_file = open("src/baseDatos/temp/lista_cuentas.pkl", "wb")
+            objects_file = open(cls.route_db + "\lista_cuentas.pkl", "wb")
             pk.dump(lista_objetos, objects_file)
             objects_file.close()
         if(isinstance(lista_objetos[0], Movimientos)):
-            objects_file = open("src/baseDatos/temp/lista_movimientos.pkl", "wb")
+            objects_file = open(cls.route_db + "\lista_movimientos.pkl", "wb")
             pk.dump(lista_objetos, objects_file)
             objects_file.close()
         if(isinstance(lista_objetos[0], Metas)):
-            objects_file = open("src/baseDatos/temp/lista_metas.pkl", "wb")
+            objects_file = open(cls.route_db + "\lista_metas.pkl", "wb")
             pk.dump(lista_objetos, objects_file)
             objects_file.close()
